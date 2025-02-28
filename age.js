4c10a68094996138dc40aef64a4d7f5ad433b859
Affan Ali <affannawab087@gmail.com>
2025-02-28 09:48:47 +0500

    Add files via upload

---
+++ age.js
@@ -0,0 +1,6 @@
+// var date = new Date().getTime();
+// var user= new Date (prompt('Enter your date of birth')).getTime();
+// var age = Math.floor((date-user) / (1000 * 60 * 60 * 24 *365));
+// document.write(age);
+
+
