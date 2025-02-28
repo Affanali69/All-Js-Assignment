e8f5805c0b69bc9f19b988ec28a57b6206ed039c
Affan Ali <affannawab087@gmail.com>
2025-02-28 10:28:13 +0500

    Update age.js

--- age.js
+++ age.js
@@ -3,4 +3,38 @@
 // var age = Math.floor((date-user) / (1000 * 60 * 60 * 24 *365));
 // document.write(age);
 
+// var number = prompt("Enter a Positive number :");
+// var roundoff = Math.round(number);
+// var floor = Math.floor(number);
+// var ceil = Math.ceil(number);
+// document.write("number is: " + number + "<br>");
+// document.write("Round off value: " + roundoff + "<br>");
+// document.write("Floor value: " + floor + "<br>");
+// document.write("Ceil value: " + ceil + "<br>");
 
+// var number = prompt("Enter a Negative number :");
+// var roundoff = Math.round(number);
+// var floor = Math.floor(number);
+// var ceil = Math.ceil(number);
+// document.write("number is: " + number + "<br>");
+// document.write("Round off value: " + roundoff + "<br>");
+// document.write("Floor value: " + floor + "<br>");
+// document.write("Ceil value: " + ceil + "<br>");
+
+// var number = parseFloat(prompt("Enter a number in decimal :"));
+// var roundoff = Math.round(number);
+// var floor = Math.floor(number);
+// var ceil = Math.ceil(number);
+// document.write("number is: " + number + "<br>");
+// document.write("Round off value: " + roundoff + "<br>");
+// document.write("Floor value: " + floor + "<br>");
+// document.write("Ceil value: " + ceil + "<br>");
+
+// var number = parseFloat(prompt("Enter a number in  Negative decimal :"));
+// var roundoff = Math.round(number);
+// var floor = Math.floor(number);
+// var ceil = Math.ceil(number);
+// document.write("number is: " + number + "<br>");
+// document.write("Round off value: " + roundoff + "<br>");
+// document.write("Floor value: " + floor + "<br>");
+// document.write("Ceil value: " + ceil + "<br>");
